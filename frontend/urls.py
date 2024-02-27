209deb283356d27fab631e74f9160ef1b876b865
hhiitthhiinn <106234950+hhiitthhiinn@users.noreply.github.com>
2024-02-27 12:14:41 +0530

    27-02-2024

---
+++ frontend/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+app_name="frontend"
+urlpatterns = [
+    path('',views.index,name="home"),
+    path("singlepage/<int:id>",views.singlepage,name="singlepage"),
+    path("category/<str:category>",views.category,name="category"),
+    path("aboutus",views.aboutus,name="aboutus"),
+    path("search",views.search,name="search"),
+    path("subscribe",views.subscribe,name="subscribe"),
+    path("contact",views.contact,name="contact"),
+    path("addquery",views.addquery,name="addquery"),
+    path("recipies",views.recipies,name="recipies")
+]
